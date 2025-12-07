5d4f26bb9fdb4fe600d5869429b8120f16516eb0
mickostrn <mickostrn@gmail.com>
2025-12-08 06:29:35 +0700

    menambahkan responsive laptop dan hp

--- index.js
+++ index.js
@@ -5,10 +5,3 @@ document.querySelector("#CTA").addEventListener("click", () => {
     document.querySelector("#message").value = "";
   }, 5);
 });
-document.querySelector("#CTA").addEventListener("click", () => {
-  setTimeout(() => {
-    document.querySelector("#name").value = "";
-    document.querySelector("#email").value = "";
-    document.querySelector("#message").value = "";
-  }, 5);
-});
